ca473b35b3c48fc6fe9c49006b81c74cb135a32d
Kaique Demetrio <160547555+kaiquedm12@users.noreply.github.com>
2025-11-28 12:23:53 -0300

    criando spring-boot

---
+++ EventFlow-backend/EventFlow/src/main/java/com/example/EventFlow/EventFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFlowApplication.class, args);
+	}
+
+}
---
+++ EventFlow-backend/EventFlow/src/test/java/com/example/EventFlow/EventFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
