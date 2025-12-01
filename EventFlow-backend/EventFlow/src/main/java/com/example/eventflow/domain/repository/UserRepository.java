7eeb8f903d2210635891889fd6fb2542197ad09a
Kaique Demetrio <160547555+kaiquedm12@users.noreply.github.com>
2025-11-30 21:23:34 -0300

    criei o docker com o postgres mas ainda não salva no banco

--- EventFlow-backend/EventFlow/src/main/java/com/example/eventflow/domain/repository/UserRepository.java
+++ EventFlow-backend/EventFlow/src/main/java/com/example/eventflow/domain/repository/UserRepository.java
@@ -1,7 +1,9 @@
-package com.example.eventflow.domain.repository;
+package com.example.eventflow.repository;
 
 import com.example.eventflow.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
